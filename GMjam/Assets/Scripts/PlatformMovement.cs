dc9934a29fd3ce8c522ae402dd377af23c9078a7
Joao Pedro Fabiani <jp-fabiani@hotmail.com>
2019-08-03 11:34:29 -0300

    Plataformas que se movem
    Ainda há um problema no qual as plataformas que se movem horizontalmente não levam o player junto.

---
+++ GMjam/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+
+    private Vector3 StartPoint;
+    public Vector3 EndPoint;
+    public float speed;
+    public float waitTime;
+    private Vector3 temp;
+    private float StartTime;
+    private float distancia;
+    private float distanciaPercorrida;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartTime = Time.time;
+        StartPoint = transform.position;
+        //EndPoint = new Vector3(7.54f, 1.92f, 0);
+        distancia = Vector3.Distance(StartPoint, EndPoint);
+        waitTime = 2f;
+        StartCoroutine(Timer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanciaPercorrida = (Time.time - StartTime) * speed;
+        float lerpFrac = distanciaPercorrida / distancia;
+        transform.position = Vector3.Lerp(StartPoint, EndPoint, lerpFrac);
+
+    }
+
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSecondsRealtime(waitTime);
+        temp = StartPoint;
+        StartPoint = EndPoint;
+        EndPoint = temp;
+        //EndPoint = new Vector3(0.32f, 1.92f, 0);
+        distanciaPercorrida = 0;
+        StartTime = Time.time;
+
+        yield return new WaitForSecondsRealtime(waitTime);
+        temp = StartPoint;
+        StartPoint = EndPoint;
+        EndPoint = temp;
+
+        // EndPoint = new Vector3(7.3f, 1.92f, 0);
+        distanciaPercorrida = 0;
+        StartTime = Time.time;
+
+        StartCoroutine(Timer());
+
+
+    }
+}
---
+++ GMjam/Assets/Scripts/PlatformMovement1.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement1 : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Vector3 StartPoint;
+
+    public Vector3 EndPoint;
+    private Vector3 temp;
+    public float speed;
+
+    private float waitTimeTimer;
+    public float waitTime;
+
+    private Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        StartPoint = transform.position;
+        waitTimeTimer = waitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+
+    public void Move()
+    {
+        if (Vector3.Distance(transform.position, EndPoint) > 0.2)
+        {
+            
+            direction = (EndPoint - StartPoint).normalized;
+            rb.velocity = direction * speed;
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+            if (waitTimeTimer < 0)
+            {
+                temp = EndPoint;
+                EndPoint = StartPoint;
+                StartPoint = temp;
+                waitTimeTimer = waitTime;
+            }
+            else
+            {
+                waitTimeTimer -= Time.deltaTime;
+            }
+            
+        }
+       
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            col.GetComponent<Rigidbody2D>().velocity = new Vector3(100, 0, 0);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            col.transform.parent = null;
+        }
+    }
+}
